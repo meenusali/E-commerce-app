35a6ad4d9e7ffd5a8236f3cb1eb988b18182805b
meenusali <meenusali98@gmail.com>
2025-04-26 16:33:44 +0530

    product listing

--- frontend full stack/src/main.jsx
+++ frontend full stack/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
+import ShopContextProvider, { ShopContext } from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+  <ShopContextProvider>
+   <App />
+  </ShopContextProvider>
+    
   </BrowserRouter>,
 )
